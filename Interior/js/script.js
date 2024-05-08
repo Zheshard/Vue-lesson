1c706a8cb38293f280ddfccaa5a7bea4980bc5ee
Zheshard <antonzestkov489@gmail.com>
2024-05-08 17:52:34 +0300

    homework 2 complete

---
+++ Interior/js/script.js
@@ -0,0 +1,62 @@
+new Vue({
+    el: '.app',
+    data: {
+        title: "Articles & News",
+        articles: [
+            {
+                id: 0,
+                image: "img/index/blog/Image1.jpeg",
+                span: "Kitchan Design",
+                title: "Let’s Get Solution For Building Construction Work",
+                date: "26 December,2022",
+                vector: "img/index/projects/Vector.svg",
+            },
+            {
+                id: 1,
+                image: "img/index/blog/Image2.jpeg",
+                span: "Living Design",
+                title: "Low Cost Latest Invented Interior Designing Ideas",
+                date: "26 December,2022",
+                vector: "img/index/projects/Vector.svg",
+            },
+            {
+                id: 2,
+                image: "img/index/blog/Image3.jpeg",
+                span: "Kitchan Design",
+                title: "Let’s Get Solution For Building Construction Work",
+                date: "26 December,2022",
+                vector: "img/index/projects/Vector.svg",
+            },
+            {
+                id: 3,
+                image: "img/index/blog/Image4.jpeg",
+                span: "Kitchan Design",
+                title: "Let’s Get Solution For Building Construction Work",
+                date: "26 December,2022",
+                vector: "img/index/projects/Vector.svg",
+            },
+            {
+                id: 4,
+                image: "img/index/blog/Image5.jpeg",
+                span: "Kitchan Design",
+                title: "Let’s Get Solution For Building Construction Work",
+                date: "26 December,2022",
+                vector: "img/index/projects/Vector.svg",
+            },
+            {
+                id: 5,
+                image: "img/index/blog/Image6.jpeg",
+                span: "Kitchan Design",
+                title: "Let’s Get Solution For Building Construction Work",
+                date: "26 December,2022",
+                vector: "img/index/projects/Vector.svg",
+            },
+        ],
+    },
+
+    methods: {
+    },
+
+    computed: {
+    },
+});
