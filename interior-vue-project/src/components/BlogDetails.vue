2b8c75d31fd3204e69ce4ebb1ac36448d18db8a5
Zheshard <antonzestkov489@gmail.com>
2024-05-11 12:41:27 +0300

    homework 4 vue cli

---
+++ interior-vue-project/src/components/BlogDetails.vue
@@ -0,0 +1,149 @@
+<template>
+    <div class="details center">
+            <div>
+                <div class="details__content" v-for="(item, index) in filterArticles" :key="index">
+                    <article class="content__article-content article-content">
+                        <h2 class="article-content__title">{{item.title}}</h2>
+                        <div class="article-content__image">
+                            <img :src="item.image" alt="Phtoto">
+                        </div>
+                        <p class="article-content__date">
+                            <span>{{item.date}}</span>
+                            <span>{{item.link}}</span>
+                        </p>
+                        <div class="article-content__text" v-for="(paragraph, index) in item.text" :key="index">
+                            <p class="article-content__paragraph">{{paragraph}}</p>
+                        </div>
+                        <div class="article-content__quotes">
+                            <img src="../assets/img/blog_details/“.svg" alt="">
+                            <blockquote>{{item.quotes}}</blockquote>
+                        </div>
+                    </article>
+                </div>
+            </div>
+
+            <div>
+                <aside class="details__tags tags">
+                    <h3 class="tags__title">Tags</h3>
+                    <div class="tags__select" v-for="(tag, index) in tags" :key="index">
+                        <div class="tags__btn">
+                            <input :id="tag.tagName" type="radio" name="radio" :value="tag.tagName" @click="filteredListArticles(tag.tagName)">
+                            <label :for="tag.tagName">{{tag.tagName}}</label>
+                        </div>
+                    </div>
+                </aside>
+            </div>
+        </div>
+  </template>
+
+<script>
+export default {
+  name: 'BlogDetails',
+  data () {
+    return {
+      filterArticles: [
+        {
+          tag: 'Kitchen',
+          title: 'Let’s Get Solution for Building Construction Work',
+          image: require('../assets/img/blog_details/Photo.jpeg'),
+          date: '26 December,2022',
+          link: 'Interior / Home / Decore',
+          text: ["Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae turpmaximus.posuere in.Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injecthumour, or randomised words which don't look even slightly believable.",
+            'Embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary.',
+            'Embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary.'],
+          quotes: 'The details are not the details. They make the design.'
+        },
+        {
+          tag: 'Bedroom',
+          title: 'Bedroom',
+          image: require('../assets/img/blog_details/Photo.jpeg'),
+          date: '26 December,2022',
+          link: 'Interior / Home / Decore',
+          text: ["Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae turpmaximus.posuere in.Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injecthumour, or randomised words which don't look even slightly believable.",
+            'Embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary.'],
+          quotes: 'The details are not the details. They make the design.'
+        }
+      ],
+      articles: [
+        {
+          tag: 'Kitchen',
+          title: 'Let’s Get Solution for Building Construction Work',
+          image: require('../assets/img/blog_details/Photo.jpeg'),
+          date: '26 December,2022',
+          link: 'Interior / Home / Decore',
+          text: ["Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae turpmaximus.posuere in.Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injecthumour, or randomised words which don't look even slightly believable.",
+            'Embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary.',
+            'Embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary.'],
+          quotes: 'The details are not the details. They make the design.'
+        },
+        {
+          tag: 'Bedroom',
+          title: 'Bedroom',
+          image: require('../assets/img/blog_details/Photo.jpeg'),
+          date: '26 December,2022',
+          link: 'Interior / Home / Decore',
+          text: ["Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae turpmaximus.posuere in.Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injecthumour, or randomised words which don't look even slightly believable.",
+            'Embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary.'],
+          quotes: 'The details are not the details. They make the design.'
+        },
+        {
+          tag: 'Building',
+          title: 'Building',
+          image: require('../assets/img/blog_details/Photo.jpeg'),
+          date: '26 December,2022',
+          link: 'Interior / Home / Decore',
+          text: ["Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae turpmaximus.posuere in.Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injecthumour, or randomised words which don't look even slightly believable.",
+            'Embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary.'],
+          quotes: 'The details are not the details. They make the design.'
+        },
+        {
+          tag: 'Architecture',
+          title: 'Architecture',
+          image: require('../assets/img/blog_details/Photo.jpeg'),
+          date: '26 December,2022',
+          link: 'Interior / Home / Decore',
+          text: ["Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae turpmaximus.posuere in.Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injecthumour, or randomised words which don't look even slightly believable.",
+            'Embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary.'],
+          quotes: 'The details are not the details. They make the design.'
+        },
+        {
+          tag: 'Kitchen Planning',
+          title: 'Kitchen Planning',
+          image: require('../assets/img/blog_details/Photo.jpeg'),
+          date: '26 December,2022',
+          link: 'Interior / Home / Decore',
+          text: ["Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae turpmaximus.posuere in.Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injecthumour, or randomised words which don't look even slightly believable.",
+            'Embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary.'],
+          quotes: 'The details are not the details. They make the design.'
+        },
+        {
+          tag: 'Kitchen Planning',
+          title: 'Kitchen Planning',
+          image: require('../assets/img/blog_details/Photo.jpeg'),
+          date: '26 December,2022',
+          link: 'Interior / Home / Decore',
+          text: ["Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae turpmaximus.posuere in.Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injecthumour, or randomised words which don't look even slightly believable.",
+            'Embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary.'],
+          quotes: 'The details are not the details. They make the design.'
+        }
+      ],
+      tags: [
+        { tagName: 'Kitchen' },
+        { tagName: 'Bedroom' },
+        { tagName: 'Building' },
+        { tagName: 'Architecture' },
+        { tagName: 'Kitchen Planning' }
+      ]
+    }
+  },
+  methods: {
+    filteredListArticles (tagName) {
+      this.filterArticles = this.articles.filter(article => article.tag === tagName)
+    }
+  }
+}
+</script>
+
+  <style lang="scss">
+
+  </style>
---
+++ interior-vue-project/src/components/BlogPage.vue
@@ -0,0 +1,180 @@
+<template>
+    <div>
+        <header class="header center">
+        <nav class="header__nav nav">
+            <a href="index.html" class="nav__logo logo">
+                <img class="logo__icon" src="../assets/img/index/head/Logo.svg" alt="logo">
+                <h1 class="logo__title">Interno</h1>
+            </a>
+            <ul class="nav__menu">
+                <li><a href="index.html" class="nav__link">Home</a></li>
+                <li><a href="#" class="nav__link">Project</a></li>
+                <li><a href="blog.html" class="nav__link">Blog</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <div class="banner banner_blog">
+        <img src="../assets/img/blog/banner/banner.png" alt="Banner" class="banner__backgr">
+        <div class="center">
+            <div class="banner_blog__heading">
+                <h1 class="banner_blog__title">Articles & News</h1>
+                <h2 class="banner_blog__subtitle">Home / Blog</h2>
+            </div>
+        </div>
+    </div>
+
+    <main class="main ">
+        <section class="latest-post center">
+            <h2 class="latest-post__title title-blog">Latest Post</h2>
+            <div class="latest-post__post post">
+                <div class="post__image">
+                    <img src="../assets/img/blog/post/Image.jpeg" alt="Picture">
+                </div>
+                <div class="post__text-block">
+                    <h3 class="post__title">
+                        Low Cost Latest Invented Interior Designing Ideas
+                    </h3>
+                    <div class="post__text">
+                        <p>
+                            Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae turpis dignissim maximus.posuere
+                            in.Contrary to popular belief.
+                        </p>
+                        <p>Lorem Ipsum is not simply random text. It has roots in a piece of classica.</p>
+                    </div>
+                    <div class="article__row">
+                        <span class="article__date">25 December,2022 </span>
+                        <a href="#" class="article__link link-vector">
+                            <img src="../assets/img/index/projects/Vector.svg" alt="Vector">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <div class="app">
+            <section class="blog center">
+                <h2 class="blog__title title-blog">{{title}}</h2>
+                <div class="blog__articles">
+                    <div v-for="(item, index) in articles" :key="index">
+                        <article class="blog__article article">
+                            <div class="article__picture">
+                                <img :src="item.image" alt="Image1">
+                                <span>{{item.span}}</span>
+                            </div>
+                            <h3 class="article__title">{{item.title}}</h3>
+                            <div class="article__row">
+                                <span class="article__date">{{item.date}}</span>
+                                <a href="blog_details.html" class="article__link link-vector">
+                                    <img :src="item.vector" alt="Vector">
+                                </a>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </main>
+
+    <footer class="footer center">
+        <div class="footer__left-part">
+            <a href="index.html" class="footer__logo logo">
+                <img class="logo__icon" src="../assets/img/index/head/Logo.svg" alt="logo">
+                <h1 class="logo__title">Interno</h1>
+            </a>
+            <p class="footer__text">
+                It is a long established fact that a reader will be distracted lookings.
+            </p>
+            <div class="footer__icons">
+                <a href="#" class="footer_icon1">
+                    <img src="../assets/img/index/footer/twitter.svg" alt="twitter.svg">
+                </a>
+                <a href="#" class="footer_icon2">
+                    <img src="../assets/img/index/footer/in.svg" alt="in.svg">
+                </a>
+            </div>
+        </div>
+
+        <div class="footer__links">
+            <h4 class="footer__title">Pages</h4>
+            <ul class="footer__nav">
+                <li><a href="index.html" class="footer__link">Home</a></li>
+                <li><a href="#" class="footer__link">Project</a></li>
+                <li><a href="blog.html" class="footer__link">Blog</a></li>
+            </ul>
+        </div>
+
+        <div class="footer__contact">
+            <h4 class="footer__title">Contact</h4>
+            <span>55 East Birchwood Ave. Brooklyn, New York 11201</span>
+            <span><a href="mailto:contact@interno.com">contact@interno.com</a></span>
+            <span>(123) 456 - 7890</span>
+        </div>
+    </footer>
+    </div>
+  </template>
+
+<script>
+export default {
+  name: 'BlogPage',
+  data () {
+    return {
+      title: 'Articles & News',
+      articles: [
+        {
+          id: 0,
+          image: require('../assets/img/index/blog/Image1.jpeg'),
+          span: 'Kitchan Design',
+          title: 'Let’s Get Solution For Building Construction Work',
+          date: '26 December,2022',
+          vector: require('../assets/img/index/projects/Vector.svg')
+        },
+        {
+          id: 1,
+          image: require('../assets/img/index/blog/Image2.jpeg'),
+          span: 'Living Design',
+          title: 'Low Cost Latest Invented Interior Designing Ideas',
+          date: '26 December,2022',
+          vector: require('../assets/img/index/projects/Vector.svg')
+        },
+        {
+          id: 2,
+          image: require('../assets/img/index/blog/Image3.jpeg'),
+          span: 'Kitchan Design',
+          title: 'Let’s Get Solution For Building Construction Work',
+          date: '26 December,2022',
+          vector: require('../assets/img/index/projects/Vector.svg')
+        },
+        {
+          id: 3,
+          image: require('../assets/img/index/blog/Image4.jpeg'),
+          span: 'Kitchan Design',
+          title: 'Let’s Get Solution For Building Construction Work',
+          date: '26 December,2022',
+          vector: require('../assets/img/index/projects/Vector.svg')
+        },
+        {
+          id: 4,
+          image: require('../assets/img/index/blog/Image5.jpeg'),
+          span: 'Kitchan Design',
+          title: 'Let’s Get Solution For Building Construction Work',
+          date: '26 December,2022',
+          vector: require('../assets/img/index/projects/Vector.svg')
+        },
+        {
+          id: 5,
+          image: require('../assets/img/index/blog/Image6.jpeg'),
+          span: 'Kitchan Design',
+          title: 'Let’s Get Solution For Building Construction Work',
+          date: '26 December,2022',
+          vector: require('../assets/img/index/projects/Vector.svg')
+        }
+      ]
+    }
+  }
+}
+</script>
+
+  <style lang="scss">
+
+  </style>
